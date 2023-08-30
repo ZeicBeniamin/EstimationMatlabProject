938c64fa775f5c034a60cdda59c7902c42c07911
ZeicBeniamin <beniamin222052000@gmail.com>
2023-08-30 16:59:35 +0300

    Before last part

--- nonlinear.m
+++ nonlinear.m
@@ -22,35 +22,3 @@ x3_dot = 1/((fiP1/fiP2)*exp(-x1/fiP2))*(ki*u+ci-x3);
 dx = [x1_dot; x2_dot; x3_dot];
 
 end
-% 
-% clear xls_kn xls_kp store samples iter
-% iter = 500;
-% Dx0 = [0.004; 
-%         -0.15;  
-%         2.5];
-% xns_n = x0 + Dx0;
-% store = zeros(3, iter);
-% samples = 1:iter;
-% for i = 1:iter
-%     xns_p = xns_n;
-%     store(:, i) = xns_n;
-%     xns_n = xns_p + Ts * nonlinear(xns_p, (-Kd * (xns_p - x0) + u0)); % Ad * xns_p + Bd * (-K*x);
-% end
-% 
-% 
-% xlo_n = Dx0;
-% xlo_hat_n = Dxh0;
-% store = zeros(3, iter);
-% 
-% for i = 1:iter
-%     xlo_p = xlo_n;
-%     xlo_hat_p = xlo_hat_n;
-% 
-%     store(:, i) = xlo_n + x0;
-% 
-%     xlo_n = Ad * xlo_p + Bd * (-Kd * xlo_hat_p);
-%     err = C * (xlo_p - xlo_hat_p);
-%     xlo_hat_n = Ad * xlo_hat_p + Bd * (-Kd * xlo_hat_p) + Ld * err;
-% end
-% 
-% % figure
